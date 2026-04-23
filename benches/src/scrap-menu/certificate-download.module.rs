73930329e8e0b327b9c802b3195f8802844561e0
FEDDIE <aminuaustin22@gmail.com>
2026-04-23 12:47:19 +0100

    Delete benches directory

--- benches/src/scrap-menu/certificate-download.module.rs
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CertificateDownloadController } from './certificate-download.controller';
-import { CertificateDownloadService } from './certificate-download.service';
-
-@Module({
-  controllers: [CertificateDownloadController],
-  providers: [CertificateDownloadService],
-})
-export class CertificateDownloadModule {}
